34e1b24c1cee396706d8e4f76b00acdca6ac6841
FeridPiriyev <smdpiriyev@gmail.com>
2023-12-05 13:49:12 +0400

    e-olymp mesele 8521 add

---
+++ If, else, elif structure/algoritm10.py
@@ -0,0 +1,17 @@
+'''
+Növbəti şərtə uyğun olaraq y-in qiymətini hesablayın:
+y={ 
+1.x**3+5x,x>=10
+2.x**2-2x,x<10
+}
+
+e-olymp mesele 8521
+'''
+
+x=int(input())
+if x>=10:
+    y=pow(x,3)+5*x
+    print(y)
+else:
+    y=pow(x,2)+2*x
+    print(y)
