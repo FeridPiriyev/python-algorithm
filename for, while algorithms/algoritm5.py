9446145621c3739f5db4288f18a778ef9e611a15
FeridPiriyev <smdpiriyev@gmail.com>
2023-12-12 09:13:43 +0400

    algoritm5

---
+++ for, while algorithms/algoritm5.py
@@ -0,0 +1,17 @@
+num1=int(input())
+num2=int(input())
+num1_copy=num1
+num2_copy=num2
+num_say=0
+while num1!=num2:
+    if num1>num2:
+        num1=num1-num2
+    else:
+        num2=num2-num1
+ebob=num1
+ekob=(num1_copy*num2_copy)//ebob
+while ebob<ekob:
+    ebob=ebob+1
+    if ebob%3==1:
+        num_say=num_say+1
+print(num_say)
