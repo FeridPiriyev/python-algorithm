e08a177009c7239a4d1028bf5e9fdc2c21ea983c
FeridPiriyev <smdpiriyev@gmail.com>
2023-12-19 14:54:27 +0400

    ici bosh duzbucaqli uchbucaq

---
+++ for, while algorithms/algoritm7.py
@@ -0,0 +1,13 @@
+'''
+Hundurluk verilir ici bosh duzbucaqli ucbucaq quran proqram
+#
+##
+# #
+#  #
+#####
+'''
+h=int(input())
+print("#")
+for i in range(1,h-1):
+    print("#"+(i-1)*" "+"#")
+print((i+2)*"#")
